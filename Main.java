3fd945830d30fbffd91408cb65fbc8c36d134733
ラハマン <rahman.just16@gmail.com>
2026-02-12 21:38:53 +0900

    area

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
 
 import java.util.Scanner;
 
+
+
 public class Main {
     public static void main (String [] args){
-        System.out.println("Test Math Operation");
-       Scanner myobj= new Scanner(System.in);
-       System.out.println("Enter first number:");
-       int n= myobj.nextInt();
-       System.out.println(n);
       
+      System.out.println("--------Area of Circcle--------");
+       double pi=3.1416;
+       double area, radius;
+       
+       System.out.println("Enter the radius of the circle: ");
+       Scanner r= new Scanner(System.in);
+       radius = r.nextDouble();
+       area= pi * radius * radius;
+
+
+       System.out.printf("The area of the circle is: %.2f",area);
+
+
+
+       
+
 
         
 
